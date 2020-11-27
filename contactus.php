b4db144cbb83358e004d49759fd972579e057b1b
georgeitis <74385677+georgeitis@users.noreply.github.com>
2020-11-27 08:39:06 -0500

    Add files via upload
    contactus & rules

---
+++ contactus.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <style>
+    body {
+         background-color: lightblue;
+    }
+    </style>
+</head>
+
+<body>
+
+Name <br> <?php echo $_POST["name"]; ?><br><br>
+
+Email <br>  <?php echo $_POST["Email1"]; ?><br><br>
+
+Text <br> <?php echo $_POST["text"]; ?><br>
+
+</body>
+</html>
