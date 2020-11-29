e7cd156b2cc415aee33cb637c3e7e792363d342b
georgeitis <74385677+georgeitis@users.noreply.github.com>
2020-11-29 17:52:03 -0500

    Add files via upload

---
+++ horm.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="leader.php" method="post">
+id: <input type="number" name="id"><br>
+name: <input type="text" name="name"><br>
+score: <input type="number" name="score"><br>
+
+<input type="submit">
+</form>
+
+</body>
+</html>
